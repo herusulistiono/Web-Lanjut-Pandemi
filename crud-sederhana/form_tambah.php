fddc44a730a4be4438854e5760071618d94adbfc
Heru Sulistiono <mildlaser3@gmail.com>
2022-03-16 03:02:40 +0700

    Web Lanjut

--- crud-sederhana/form_tambah.php
+++ crud-sederhana/form_tambah.php
@@ -5,10 +5,10 @@
 </head>
 <body>
 	<form action="simpan.php" method="POST">
-		<label for="">NPM:</label>
-		<input type="text" name="txtnpm" placeholder="NPM" required="true">
-		<label for="">NAMA:</label>
-		<input type="text" name="txtnama" placeholder="NAMA" required="true">
+		<label>NPM:</label>
+		<input type="text" name="txtnpm" placeholder="NPM" required="true"/>
+		<label>NAMA:</label>
+		<input type="text" name="txtnama" placeholder="NAMA" required="true"/>
 		<button type="submit">SIMPAN</button>
 	</form>
 </body>
