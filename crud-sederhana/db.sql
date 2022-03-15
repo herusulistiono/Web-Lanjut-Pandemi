a73dde1ba2e8553e39788562c407239a7ce7c9bd
Heru Sulistiono <mildlaser3@gmail.com>
2022-03-16 02:44:34 +0700

    Web Lanjut

--- crud-sederhana/db.sql
+++ crud-sederhana/db.sql
@@ -1,1 +1,17 @@
-hello
+--
+-- Database: `web2`
+--
+
+CREATE TABLE `mahasiswa`(
+    `id` INT(8) NOT NULL AUTO_INCREMENT,
+    `npm` CHAR(20) NOT NULL,
+    `nama` VARCHAR(40) NOT NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY `npm`(`npm`)
+) ENGINE = INNODB;
+
+INSERT INTO `mahasiswa` (`id`, `npm`, `nama`) VALUES
+(1, '201401', 'Jokowi'),
+(2, '201402', 'Prabowo'),
+(3, '201403', 'Mandra'),
+(2, '201404', 'Parjo');
